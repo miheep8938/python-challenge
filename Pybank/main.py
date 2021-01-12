984dfb87cf4c7afcd9ccc581ef23505f19c917a3
Gun-Jae Jeong <you@example.com>
2021-01-12 13:35:15 -0500

    source has been changed

---
+++ Pybank/main.py
@@ -0,0 +1,14 @@
+import os 
+import csv 
+
+csvpath = os.path.join ('Resources', 'budget_data.csv')
+
+with open(csvpath) as csvfile:
+        csvreader = csv.reader(csvfile,delimiter= " ")
+
+date = []
+profit = []
+
+row_count = sum(1 for row in csvreader) 
+
+
